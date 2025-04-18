7223692e56946686f16ec3a076217e6e00e28d26
kostabkpark <kostabkpark@gmail.com>
2025-04-18 14:04:31 +0900

    last commit

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import express from "express"
 import mysql from "mysql"
+import dotenv from "dotenv"
+
+dotenv.config({path : '.env'});
 
 const app = express()
 const port = 3000
+const host = "127.0.0.1";
 
 const db = mysql.createConnection({
-  host : "127.0.0.1",
-  port: 3306,
-  user : "user_ex",
-  password : "1234",
+  host : process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user : process.env.DB_USER,
+  password : process.env.DB_PASS,
   database : "backend"
 })
 
@@ -86,36 +90,65 @@ app.post('/nations', (req,res)=>{
       res.send("등록요청 처리중 오류 발생");
     } else {
       console.log(results);
-      res.redirect(302, `http://localhost:${port}/nations`);
+      res.redirect(302, `http://${host}:${port}/nations`);
       //res.send(`${results.insertId} , ${name}, ${capital},  ${population} 에 대한 post 요청 처리 완료`);
     }
   })
-});
+}); // create
 
 app.put('/nations/:id', (req, res)=> {
-  const {id, name, capital, population } = req.body;
+  const {name, capital, population } = req.body;
+  const id = req.params.id;
     console.log(req.body);
     console.log(req.params.id);
+    const sql = "update nations_table set name = ?, capital = ?, population = ? where id = ?"
+    db.query(sql, [name, capital, population, id], (err, results)=>{
+      if(err) console.log(err);
+      //console.log(results);
+      if(results.affectedRows) {
+        console.log("수정 정상 처리 완료");
+        res.redirect(302, `http://${host}:${port}/nations/${id}`)
+      } else {
+        res.send('수정 처리중 오류 발생');
+      }
+    })
+    
+}); // PUT 보충 설명 , PATCH 와 비교
+
+
+app.patch('/nations/:id', (req, res)=> {
+  const { population } = req.body;
+  const id = req.params.id;
     const sql = "update nations_table set population = ? where id = ?"
     db.query(sql, [population, id], (err, results)=>{
       if(err) console.log(err);
-      console.log(results);
+      //console.log(results);
+      if(results.affectedRows) {
+        console.log("수정 정상 처리 완료");
+        res.redirect(302, `http://${host}:${port}/nations/${id}`)
+      } else {
+        res.send('수정 처리중 오류 발생');
+      }
     })
-    res.send("처리중");
-}); // POST 보충 설명 , PATCH 와 비교
+}); // PUT 보충 설명 , PATCH 와 비교
 
 app.delete('/nations/:id', (req, res)=> {
   const id = req.params.id;
   const sql = "delete from nations_table where id = ?"
   db.query(sql, [id], (err,results) => {
     if(err) console.log(err);
-    console.log(results);
+    //console.log(results);
+    if(results.affectedRows) {
+      console.log("정상적으로 삭제 진행 완료");
+      res.redirect(302, `http://${host}:${port}/nations`);
+    } else {
+      res.send("삭제 실패");
+    }
   })
-  res.redirect(302, `http://localhost:${port}/nations`);
 });
 
 app.patch('', ()=>{});
 
-app.listen(port, "localhost", () => {
-  console.log(`localhost 도메인의 port ${port} 번에 서버 생성 후 대기 중...`)
+app.listen(port, host, () => {
+  console.log(`${host} 도메인의 port ${port} 번에 서버 생성 후 대기 중...`)
 })
